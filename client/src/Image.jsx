f97e5ab86cb35bfd702c54c24fcaa7da96ddd609
Aviral <soniaviral14@gmail.com>
2024-04-27 13:11:48 +0530

    getting images again

--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -2,6 +2,6 @@ export default function Image ({ src, ...rest }) {
   src =
     src && src.includes('https://') //for s3
       ? src
-      : 'http://localhost:4000/' + src
+      : src
   return <img {...rest} src={src} alt={''} />
 }
